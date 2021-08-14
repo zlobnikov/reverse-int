f81ac67ee0b4e0cde8c322c70a6b9065f6d4ccab
zlobnikov <daffodil@list.ru>
2021-08-14 10:25:29 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n);
+  let result = 0;
+
+  while (num) {
+    result = result * 10 + num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  return result;
 }
